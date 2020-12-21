50488a4597b0dd19b1f79d910f68f70819bbee6e
egonz993 <egonz993@gmail.com>
2020-12-20 23:25:43 -0500

    20201003_2012

---
+++ js/mFirebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCgp0yUxM7Y9gNqE7upS-ToE2MbBVqZbw8",
+    authDomain: "galeriadeopalo.firebaseapp.com",
+    databaseURL: "https://galeriadeopalo.firebaseio.com",
+    projectId: "galeriadeopalo",
+    storageBucket: "galeriadeopalo.appspot.com",
+    messagingSenderId: "401926857975",
+    appId: "1:401926857975:web:bccde0d447be71501f5ff2",
+    measurementId: "G-X8W2EKE7VK"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
